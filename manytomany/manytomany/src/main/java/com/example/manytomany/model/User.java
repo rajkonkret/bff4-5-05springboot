35f588048769dcf967787f4cd6ed886697fdd97e
rajkonkret <52714027+rajkonkret@users.noreply.github.com>
2023-05-05 18:43:18 +0200

    add many to many

---
+++ manytomany/manytomany/src/main/java/com/example/manytomany/model/User.java
@@ -0,0 +1,23 @@
+package com.example.manytomany.model;
+
+import lombok.Data;
+
+import javax.persistence.*;
+import java.util.HashSet;
+import java.util.Set;
+
+@Entity
+@Data
+@Table(name = "users")
+public class User {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    private String username;
+
+    @ManyToMany(fetch = FetchType.LAZY, cascade = {CascadeType.PERSIST, CascadeType.MERGE})
+    @JoinTable(name = "user_role", joinColumns = @JoinColumn(name = "user_id"),
+            inverseJoinColumns = @JoinColumn(name = "role_id"))
+    private Set<Role> roles = new HashSet<>();
+}
---
+++ manytomany/manytomany/src/main/java/com/example/manytomany/repository/RoleRepository.java
@@ -0,0 +1,9 @@
+package com.example.manytomany.repository;
+
+import com.example.manytomany.model.Role;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface RoleRepository extends JpaRepository<Role, Long> {
+}
